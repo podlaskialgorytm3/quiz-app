393099a913b068a98493fe32c346c820f6ca2932
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-11 22:53:40 +0100

    Add button to start quiz

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,10 @@
+export const Button = ({children}) => {
+
+    return(
+        <button
+        className="mt-20 w-52 h-16 bg-amber-500 text-xl rounded-3xl duration-700 hover:shadow-[1px_1px_8px_4px_rgba(12,5,32,0.6)]"
+        >
+        {children}
+        </button>
+    )
+}
--- src/views/StartView.jsx
+++ src/views/StartView.jsx
@@ -1,9 +1,11 @@
 import { MainContainer } from "../components/molecules/MainContainer"
 import { Title } from "../components/atoms/Title"
+import { Button } from "../components/atoms/Button"
 export const StartView = () => {
     return(
         <MainContainer>
             <Title>Welcome to the Quiz App!</Title>
+            <Button>Click to start Quiz!</Button>
         </MainContainer>
     )
 }
