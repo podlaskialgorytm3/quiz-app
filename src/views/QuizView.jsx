fbdca4d03dc1dcad50e3633f1fca33af8267521f
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-13 14:15:50 +0100

    Add interval timer to automatically switch quiz questions

--- src/views/QuizView.jsx
+++ src/views/QuizView.jsx
@@ -1,13 +1,25 @@
-import { useContext } from "react"
+import { useContext,useEffect,useState } from "react"
 import { ViewContext } from "../stores/ViewContext"
 import { MainContainer } from '../components/MainContainer'
+import questions from '../data/questions'
+
+const INTERVAL_TIME = 15000
 
 export const QuizView = () => {
+    const [currentQuestion,setCurrentQuestion] = useState(0)
     const value = useContext(ViewContext)
-    console.log(value)
+    
+    useEffect(() => {
+        const intervalTimmer = setInterval(() => {
+            setCurrentQuestion((prevIndex) => (prevIndex + 1) % questions.length)
+        },INTERVAL_TIME)
+
+        return () => clearInterval(intervalTimmer)
+    },[questions.length])
+    
     return(
         <MainContainer>
-            Quizowa pozycja ... 
+            {questions[currentQuestion].text}
         </MainContainer>
     )
 }
