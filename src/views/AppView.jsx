f40d9dd7cb4fad72380b133a5690bf2bb21ae59f
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-14 20:21:43 +0100

    Add useEffect hook and score state to AppView.jsx

--- src/views/AppView.jsx
+++ src/views/AppView.jsx
@@ -1,20 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ViewContext } from "../stores/ViewContext";
 import { StartView } from '../views/StartView'
 import { QuizView } from '../views/QuizView'
 import { ResultView } from '../views/ResultView'
 
 export const ViewContextProvider = () => {
-  const [view,setView] = useState('start')
+  const [view, setView] = useState('start')
+  const [score, setScore] = useState({
+    points: 0,
+    answears: []
+  })
+
+  const [nextView, setNextView] = useState(null);
+
+  useEffect(() => {
+    if (nextView !== null) {
+      setView(nextView);
+      setNextView(null);
+    }
+  }, [nextView]);
+
   const changeView = (newView) => {
-    setView(newView)
+    // Opóźnij aktualizację stanu na następny cykl życia przeglądarki
+    setTimeout(() => {
+      setNextView(newView);
+    }, 0);
   }
+
+  const changeScore = (points, answears) => {
+    setScore({
+      points: points,
+      answears: answears
+    })
+  }
+
   return (
-    <ViewContext.Provider value={{view,changeView}}>
+    <ViewContext.Provider value={{ view, changeView }}>
       <div className="flex flex-col justify-center items-center h-5/6">
-        {view === "start" && <StartView/>}
-        {view === "quiz" && <QuizView />}
-        {view === "result" && <ResultView />}
+        {view === "start" && <StartView />}
+        {view === "quiz" && <QuizView changeScore={changeScore} />}
+        {view === "result" && <ResultView score={score} />}
       </div>
     </ViewContext.Provider>
   );
