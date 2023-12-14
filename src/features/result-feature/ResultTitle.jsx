be7588e5990359f017ab6235a3da362cd1befbdd
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-14 18:58:20 +0100

    Add ResultTitle component to ResultView

---
+++ src/features/result-feature/ResultTitle.jsx
@@ -0,0 +1,7 @@
+export const ResultTitle = ({children}) => {
+    return (
+        <h1 className="text-6xl font-bold uppercase mt-6 text-fuchsia-950">
+            {children}
+        </h1>
+    )
+}
--- src/views/ResultView.jsx
+++ src/views/ResultView.jsx
@@ -1,9 +1,11 @@
 import { MainContainer } from "../components/MainContainer"
 import IMG from '../assets/quiz-complete.png'
+import { ResultTitle } from "../features/result-feature/ResultTitle"
 export const ResultView = () => {
     return(
         <MainContainer>
              <img src={IMG} alt="quiz complete" className="w-[150px] h-[150px]"/>
+             <ResultTitle>Quiz Completed!</ResultTitle>
         </MainContainer>
     )
 }
