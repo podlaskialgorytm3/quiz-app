790b9b18dad68dca13fcc68cd31092284ce6ec70
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-14 22:33:31 +0100

    Get user score

--- src/views/ResultView.jsx
+++ src/views/ResultView.jsx
@@ -1,15 +1,12 @@
+import React, { useEffect, useState } from "react";
 import { MainContainer } from "../components/MainContainer"
 import IMG from '../assets/quiz-complete.png'
 import { ResultTitle } from "../features/result-feature/ResultTitle"
+import questions from "../data/questions"
+
+
 export const ResultView = ({score}) => {
-<<<<<<< HEAD
        return(
-=======
-    score.answears.forEach((answear, index) => {
-        console.log(answear, index)
-    })
-    return(
->>>>>>> parent of 172aa69 (Show data from user)
         <MainContainer>
              <img src={IMG} alt="quiz complete" className="w-[150px] h-[150px]"/>
              <ResultTitle>Quiz Completed!</ResultTitle>
