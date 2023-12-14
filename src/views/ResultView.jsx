ceeab53ce9967dd819428fcb1850d0a0dd85822b
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-14 22:38:10 +0100

    Refactor ResultView component to display quiz completion message and score

--- src/views/ResultView.jsx
+++ src/views/ResultView.jsx
@@ -7,23 +7,12 @@ import questions from "../data/questions"
 
 export const ResultView = ({score}) => {
     console.log(score)
-       return(
+    const percentage = (score / (questions.length - 1)) * 100;
+    return(
         <MainContainer>
-             <img src={IMG} alt="quiz complete" className="w-[150px] h-[150px]"/>
-             <ResultTitle>Quiz Completed!</ResultTitle>
-             {/* {
-                score.answears.forEach((answear, index) => {
-                if(answear === questions[index].correctIndex){
-                (<p>Poprawna odpowiedź!</p>)
-                }
-                 else if(answear == "dull"){
-                (<p>Nie udzielono odpowiedzi!</p>)
-                }
-                else{
-                (<p>Błędna odpowiedź!</p>)
-                }
-                })
-             } */}
+            <img src={IMG} alt="quiz complete" className="w-[150px] h-[150px]"/>
+            <ResultTitle className="text-3xl font-bold text-center mb-4">Quiz Completed!</ResultTitle>
+            <p className="text-center text-4xl mt-10 font-bold shadow">You scored {percentage}%</p>
         </MainContainer>
     )
 }
